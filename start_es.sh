0b229b0ff73df0b5d204c4443e5b1b8ad7ec67ec
Vic Hargrave <vichargrave@gmail.com>
2016-07-15 11:23:32 -0700

    Updated to handle kibana

--- start_es.sh
+++ start_es.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Creating gsk-search services..."
+echo "Creating gsk-search and gsk-kibana services..."
 kubectl create -f service-account.yaml
 kubectl create -f es-discovery-svc.yaml
 kubectl create -f es-svc.yaml
@@ -28,3 +28,21 @@ while true; do
       fi
       sleep 2
 done
+kubectl create -f kibana-svc.yaml
+kubectl create -f kibana-rc.yaml
+while true; do
+      up=`kubectl get rc | grep kibana | awk '{print $3}'`
+      if [ "$up" != "0" ]; then
+	  break
+      fi
+      sleep 2
+done
+echo "Waiting for gsk-kibana public service IP..."
+while true; do
+    kibana_ip=`kubectl get svc gsk-kibana | grep gsk-kibana | awk '{print $3}'`
+    if [ "$kibana_ip" != "5601/TCP" ]; then
+	break
+    fi
+    sleep 2
+done
+echo "gsk-kibana public IP:    "$kibana_ip
--- stop_es.sh
+++ stop_es.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "Deleting gsk-search services..."
+echo "Deleting gsk-search and gsk-kibana services..."
 kubectl delete serviceaccount elasticsearch
 kubectl delete service gsk-search
 kubectl delete service elasticsearch-discovery
 kubectl delete replicationcontroller es-master
 kubectl delete replicationcontroller es-client
 kubectl delete replicationcontroller es-data
-
-
+kubectl delete svc gsk-kibana
+kubectl delete rc es-kibana
